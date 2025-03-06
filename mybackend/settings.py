e2a035521ee9f5dff56226e5ba2492e7e15602f4
Amory <abotha181@gmail.com>
2025-03-06 15:40:31 +0200

    hosts

--- mybackend/settings.py
+++ mybackend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY', default='your-default-secret-key')
 
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ["127.0.0.1","localhost","gmawebappbackend.onrender.com"]
+ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
